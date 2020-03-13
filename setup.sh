a0209cf716262e6ef0899edc0acd03c9b3f3906b
NolanR <ngr@uoregon.edu>
2020-03-12 23:11:59 -0700

    user prompts better in setup.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 # Installs required prerequisites for running corona_model.py
 
 # Confirms if user wants to install new software
-printf "This program installs prerequsites for this repository: Python3, pip3, \python3-lib-pandas, python3-lib-matplotlib, \n
-and python3-lib-sklearn will be installed if these files do not exist. This is preceeded by a system update. All of this \n
+printf "This program installs prerequsites for this repository: Python3, pip3, python3-lib-pandas, python3-lib-matplotlib, 
+and python3-lib-sklearn will be installed if these files do not exist. This is preceeded by a system update. All of this 
 will not be harmful to your computer apart from storage issues.\n";
 echo -n "Do you wish to proceed? [y / n] ";
 read userInput;
