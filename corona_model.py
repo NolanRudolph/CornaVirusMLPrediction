eab8881cf96db2948e80e07bf0fec0a7c9d01324
NolanR <ngr@uoregon.edu>
2020-03-12 21:07:14 -0700

    Added comments

--- corona_model.py
+++ corona_model.py
@@ -6,54 +6,58 @@ Created on Mon Mar  9 21:52:48 2020
 @author: Jacob Evarts, Nolan Rudolph
 """
 
+# Quick processing
 import numpy as np
+
+# CSV Reading
 import pandas as pd
+
+# Plotting
 import matplotlib.pyplot as plt
+
+# Label encoder takes each region and converts it to a unique number for each region
+# One Hot makes a column for every region (for boolean value) -- Adds new Column for every region
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 """--- Data Acquisition ---"""
 # Importing the dataset
 dataset = pd.read_csv('novel-corona-virus-2019-dataset/covid_19_data.csv')
 
-# Right now model predicts number of deaths due to novel coronavirus
-confirmed_tseries = pd.read_csv('novel-corona-virus-2019-dataset/time_series_covid_19_confirmed.csv')
-recovered_tseries = pd.read_csv('novel-corona-virus-2019-dataset/time_series_covid_19_recovered.csv')
-death_tseries = pd.read_csv('novel-corona-virus-2019-dataset/time_series_covid_19_deaths.csv')
-
-# Dataframe of relevant features 
-df = pd.DataFrame()
-df = dataset.copy()
+# Dataframe of relevant features -- Matrix that is really easy to work with
+dataFrame = pd.DataFrame()
+dataFrame = dataset.copy()
 
 # Replaces unknown province/state with the country name
-for index, row in df.iterrows():
-    if pd.isna(df.iloc[index, 2]):
-        df.iloc[index, 2] = df.iloc[index, 3]
+for index, row in dataFrame.iterrows():
+    if pd.isna(dataFrame.iloc[index, 2]):
+        dataFrame.iloc[index, 2] = dataFrame.iloc[index, 3]
 
-df.insert(3, "Region", df.loc[:, "Province/State"])
-df = df.rename(columns={"Province/State" : "Encoded_Region"})
+dataFrame.insert(3, "Region", dataFrame.loc[:, "Province/State"])
+dataFrame = dataFrame.rename(columns={"Province/State" : "Encoded_Region"})
 
 """--- Encode Categorical Data ---"""
 # sklearn
 label_encoder = LabelEncoder()
-regions = df.loc[:, "Region"]
+regions = dataFrame.loc[:, "Region"]
 # Just the Province/State column
 # Encodes Province/State as numbers
-df.loc[:, "Encoded_Region"] = label_encoder.fit_transform(df.loc[:, "Encoded_Region"])
+dataFrame.loc[:, "Encoded_Region"] = label_encoder.fit_transform(dataFrame.loc[:, "Encoded_Region"])
 
 # Creates dummy variables for the Province/States
+# Each column represents one region (boolean values) due to HotEncoder
 onehotencoder = OneHotEncoder()
 regions = regions.values.reshape(-1, 1)
 regions = onehotencoder.fit_transform(regions).toarray()
 column_names = onehotencoder.get_feature_names(['Region'])
-regions_df = pd.DataFrame(regions, columns = column_names)
+regions_dataFrame = pd.DataFrame(regions, columns = column_names)
 
-df = pd.concat([df, regions_df], axis=1)
+dataFrame = pd.concat([dataFrame, regions_dataFrame], axis=1)
 # drop last column of encoding for dependancy issues
-df = df.iloc[:,:-1]
+dataFrame = dataFrame.iloc[:,:-1]
 
 """--- Final Preprocessing ---"""
 # Convert dates into numerical value (days since first date given)
-dates = df.loc[:, "ObservationDate"]
+dates = dataFrame.loc[:, "ObservationDate"]
 num_days = pd.Series()
 for date in dates:
     month = date[0:2]
@@ -66,22 +70,22 @@ for date in dates:
         days_since = (int(day) + 9)
     num_days = num_days.append(pd.Series(days_since), ignore_index=True)
 
-df.insert(2, "Days_Elapsed", num_days)
+dataFrame.insert(2, "Days_Elapsed", num_days)
 
 # Dataframe with unnecessary features removed
-df_model = df.copy()
-df_model.drop(["SNo", "ObservationDate", "Encoded_Region", "Region", \
+dataFrame_model = dataFrame.copy()
+dataFrame_model.drop(["SNo", "ObservationDate", "Encoded_Region", "Region", \
                "Country/Region", "Last Update"], inplace=True, axis=1)
     
 
 """--- Test/Train Split ---"""
 from sklearn.model_selection import train_test_split
+# X_test - 70% of data, X_val - 10% of data, X_test - 20% of data
 # Splits data into train/validation data and labels, and test data
-X_train_val, X_test, y_train_val, y_test = train_test_split(df_model, df_model["Deaths"], test_size = 0.2, random_state = 0)
+X_train_val, X_test, y_train_val, y_test = train_test_split(dataFrame_model, dataFrame_model["Deaths"], test_size = 0.2, random_state = 0)
 
 # Splits taining and validation data
 X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val, test_size = 0.125, random_state = 0)
-# X_test - 70% of data, X_val - 10% of data, X_test - 20% of data
 
 
 """--- Model ---"""
@@ -167,13 +171,13 @@ plt.show()
 ***UNCESSESARY SHIT WE PROBABLY DONT NEED***
 
 
-df = confirmed_tseries.copy()
-df = df.rename(columns={"Province/State" : "Region"})
+dataFrame = confirmed_tseries.copy()
+dataFrame = dataFrame.rename(columns={"Province/State" : "Region"})
 
 # Replaces unknown province/state with the country name
-for index, row in df.iterrows():
-    if pd.isna(df.iloc[index, 0]):
-        df.iloc[index, 0] = df.iloc[index, 1]
+for index, row in dataFrame.iterrows():
+    if pd.isna(dataFrame.iloc[index, 0]):
+        dataFrame.iloc[index, 0] = dataFrame.iloc[index, 1]
 
 # Rename date columns to specify they refer to the number of confirmed cases
 for i in range(30):
@@ -186,7 +190,7 @@ for i in range(30):
     else:
         date += ("2/")
         date += (str(i - 9) + "/20")
-    df = df.rename(columns={date : ("conf-" + date)})
+    dataFrame = dataFrame.rename(columns={date : ("conf-" + date)})
     
 # Add number of recovered individuals on each date
 for i in range(30):
@@ -199,7 +203,7 @@ for i in range(30):
     else:
         date += ("2/")
         date += (str(i - 9) + "/20")
-    df["rec-" + date] = recovered_tseries[date]
+    dataFrame["rec-" + date] = recovered_tseries[date]
     
 # Add number of individuals that died on each date
 for i in range(30):
@@ -212,12 +216,12 @@ for i in range(30):
     else:
         date += ("2/")
         date += (str(i - 9) + "/20")
-    df["dead-" + date] = recovered_tseries[date]
+    dataFrame["dead-" + date] = recovered_tseries[date]
     
-df.drop("Country/Region", inplace=True, axis=1)
+dataFrame.drop("Country/Region", inplace=True, axis=1)
 
 # Remove region text data for model
-df_model = df.drop("Region", axis=1)
+dataFrame_model = dataFrame.drop("Region", axis=1)
 """
     
 
