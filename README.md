38abab1063cd7e5781d09fe1f7e620a4f4f5f81b
NolanR <ngr@uoregon.edu>
2020-03-12 23:02:05 -0700

    Added README and setup script

---
+++ README.md
@@ -0,0 +1,24 @@
+<p align="center">
+  <b>A Macro-Metric Machine Learning Prediction for CoronaVirus Propagation</b>
+</p>
+
+**Requirements** 
+1. Python3 Numpy
+2. Python3 Pandas
+3. Python3 Matplotlib
+4. Python3 SciKit-Learn
+  
+(Note: These requirements are fulfilled by following **Setup**)  
+  
+**Setup**
+1. $ git clone https://github.com/NolanRudolph/CornaVirusMLPrediction.git
+2. $ cd CoronaVirusMLPrediction
+3. $ bash setup.sh
+
+**corona\_virus.py**  
+Our main script with finely tuned hyperparameters that optimize the accuracy of all future CoronaVirus regional predictions.
+
+**novel-corona-virus-2019-dataset**
+Directory filled with CSV styled data which was used to train and tune the hyperparameters of "corona\_virus.py"
+
+All personal user space tests were ran and confirmed to work on the CloudLab profile ConTools/n-Ubuntu: https://www.cloudlab.us/p/5dcc9f6c-3f8a-11e9-910b-e4434b2381fc.
---
+++ setup.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# Installs required prerequisites for running corona_model.py
+
+# Confirms if user wants to install new software
+echo "This program installs prerequsites for this repository: Python3, pip3, \
+	python3-lib-pandas, python3-lib-matplotlib, and python3-lib-sklearn \
+	will be installed if these files do not exist. This is preceeded by a \
+	system update. All of this will not be harmful to your computer apart \
+	from storage issues.";
+echo -n "Do you wish to proceed? [y / n] ";
+read userInput;
+if [[ $userInput =~ [Nn] ]]
+then
+	echo "Exiting...";
+	exit(1);
+elif ![[ $userInput =~ [Yy] ]]
+then
+	echo "Please respond with y or n. Exiting...";
+	exit(1);
+fi
+
+# Update system to not ruin user's computer
+echo "Updating...";
+sudo apt update > /dev/null;
+
+# Install python3 if not already installed
+if [ -z $(which python3) ]
+then
+	echo "Installing python3.7...";
+	sudo apt-get install -y python3 > /dev/null;
+fi
+
+# Install pip3 if not already installed
+if [ -z $(which pip3) ]
+then
+	echo "Installing pip3...";
+	sudo apt-get install -y pip3 > /dev/null;
+fi
+
+# Install pandas if not already installed
+if [ -z $(pip list | grep pandas) ]
+then
+	echo "Installing Panda...";
+	sudo pip3 install pandas > /dev/null;
+fi
+
+# Install matplotlib if not already installed
+if [ -z $(pip list | grep matplotlib) ]
+then
+	echo "Installing Matplotlib...";
+	pip3 install matplotlib > /dev/null;
+fi
+
+echo "Finished Setup!"
